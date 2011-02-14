8d5b8b082e15d82edf1de535b9384f23764c6b00
David Brown <ddb@bithead.net>
2011-02-13 16:47:25 -0800

    Initial Commit

---
+++ XcodeReader/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  XcodeReader
+//
+//  Created by David Brown on 2/13/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
