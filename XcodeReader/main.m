0adb1060d49db8628d4d1b13121bbf93b9a13bad
David Brown <ddb@bea.local>
2011-02-13 21:50:27 -0800

    test code

--- XcodeReader/main.m
+++ XcodeReader/main.m
@@ -18,14 +18,36 @@ int main (int argc, const char * argv[]) {
     
     for (id target in xcp.rootObject.targets) {
         if ([target isKindOfClass:[XPPBXNativeTarget class]]) {
-            NSLog(@"target: %@", ((XPPBXNativeTarget*)target).name);
-            for (id phase in ((XPPBXNativeTarget*)target).buildPhases) {
+            XPPBXNativeTarget* nativeTarget = (XPPBXNativeTarget*)target;
+            NSLog(@"target: %@", nativeTarget.name);
+            for (XCObject* phase in nativeTarget.buildPhases) {
+                NSLog(@"phase: %@", phase.xcodeObjectType);
+                
                 if ([phase isKindOfClass:[XPPBXSourcesBuildPhase class]]) {
                     XPPBXSourcesBuildPhase* buildPhase = (XPPBXSourcesBuildPhase*)phase;
                     for (XPPBXBuildFile* file in buildPhase.files) {
                         NSLog(@"file: %@", file.fileRef.path);
                     }
+                } else if ([phase isKindOfClass:[XPPBXResourcesBuildPhase class]]) {
+                    XPPBXResourcesBuildPhase* resourcesPhase = (XPPBXResourcesBuildPhase*)phase;
+                    for (XPPBXBuildFile* file in resourcesPhase.files) {
+                        NSLog(@"resource file: %@", file.fileRef.path);
+                    }
+                } else if ([phase isKindOfClass:[XPPBXFrameworksBuildPhase class]]) {
+                    XPPBXFrameworksBuildPhase* frameworksBuildPhase = (XPPBXFrameworksBuildPhase*)phase;
+                    for (XPPBXBuildFile* file in frameworksBuildPhase.files) {
+                        NSLog(@"framework file: %@", file.fileRef.path);
+                    }
+                } else if ([phase isKindOfClass:[XPPBXCopyFilesBuildPhase class]]) {
+                    XPPBXCopyFilesBuildPhase* copyPhase = (XPPBXCopyFilesBuildPhase*)phase;
+                    for (XPPBXBuildFile* file in copyPhase.files) {
+                        NSLog(@"copy file: %@", file.fileRef.path);
+                    }
+                } else if ([phase isKindOfClass:[XPPBXShellScriptBuildPhase class]]) {
+                    XPPBXShellScriptBuildPhase* scriptPhase = (XPPBXShellScriptBuildPhase*)phase;
+                    NSLog(@"script: %@", scriptPhase.shellScript);
                 }
+                
             }
         }
     }
